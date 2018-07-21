c0392c3f7e3b60a8fe51c7778a6285ea42d6d4f7
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-21 13:26:45 -0400

    solved testfails

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
-// create your App component here
+// create your App component her
+import React, { Component, Fragment } from 'react';
+import UUID from 'uuid'
+
+class App extends Component {
+
+    state = {
+        r: ""
+    }
+
+    display = () => {
+        return this.state.r === "" ? 
+            "hi"
+        :
+            <Fragment>
+                <h1>{this.state.r.number} astronauts</h1>
+                <ul>
+                {this.state.r.people.map( astronaut =>  <li key={UUID()}>{astronaut.name}</li>)}
+                </ul>
+            </Fragment>
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json").then(r => r.json()).then(r => this.setState({r}))
+    }
+
+    render() {
+        return (
+            <div>
+                {this.display()}
+            </div>
+        );
+    }
+}
+
+export default App;
